fad4b8a39b8c8e81cce1acef0d24e6842b910936
Shaarkzy <ibrababdulshakur2003@gmail.com>
2025-08-01 06:56:11 +0100

    Update

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pkg update
+pkg upgrade
+
+pkg install neovim
+pkg install openssh
+pkg install python3
+pkg install espeak
+pkg install fakeroot
+pkg install tor
+pkg install git
+pkg install wget
+pkg install neofetch
